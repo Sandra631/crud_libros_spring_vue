f1dd7df4ef424e8eb6788e93fe9fe630beed36e2
Sandra631 <sandra.yolotzin.reyes.garcia.com>
2025-05-18 17:18:01 -0600

    Act5-Creando metodo POST en el controlador

--- libros/src/main/java/com/sandra/libros/Controllers/LibrosController.java
+++ libros/src/main/java/com/sandra/libros/Controllers/LibrosController.java
@@ -9,6 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 import com.sandra.libros.model.LibrosModel;
 import com.sandra.libros.repository.LibrosRepository;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 
 
@@ -19,13 +22,22 @@ public class LibrosController {
     @Autowired
     private LibrosRepository librosRepository;
 
+    //Metodo Get para traer todos los datos de la base de datos
     @GetMapping("/traer-libros")
     public List<LibrosModel> TraerLibros() {
         return librosRepository.findAll();
        
     }
+    //Metodo para insertar un libro a la base de datos
+    @PostMapping("/insertar-libros")
+public LibrosModel insertarLibros(@RequestBody LibrosModel libro) {
+    return librosRepository.save(libro);
+}
+
+        
+        
+    }
     
 
 
 
-}
