537ddb230fab8449ee842eba12af85da5b3a9f9c
Sandra631 <sandra.yolotzin.reyes.garcia.com>
2025-05-18 17:36:47 -0600

    Act6-Creando metodo PUT en el controlador

--- libros/src/main/java/com/sandra/libros/Controllers/LibrosController.java
+++ libros/src/main/java/com/sandra/libros/Controllers/LibrosController.java
@@ -3,14 +3,18 @@ package com.sandra.libros.Controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sandra.libros.model.LibrosModel;
 import com.sandra.libros.repository.LibrosRepository;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import java.util.Optional;
 
 
 
@@ -33,10 +37,30 @@ public class LibrosController {
 public LibrosModel insertarLibros(@RequestBody LibrosModel libro) {
     return librosRepository.save(libro);
 }
+//Metodo para editar un libro a la base de datos
+    @PutMapping("/editar-libro/{id}")
+public ResponseEntity<LibrosModel> editarLibro(@PathVariable Long id, @RequestBody LibrosModel libroActualizado) {
+    Optional<LibrosModel> libroExistente = librosRepository.findById(id);
 
-        
-        
+    if (libroExistente.isPresent()) {
+        LibrosModel libro = libroExistente.get();
+        libro.setTitulo(libroActualizado.getTitulo());
+        libro.setAutor(libroActualizado.getAutor());
+        libro.setCategoria(libroActualizado.getCategoria());
+        libro.setDescripcion(libroActualizado.getDescripcion());
+        libro.setImagen(libroActualizado.getImagen());
+
+        LibrosModel libroGuardado = librosRepository.save(libro);
+        return ResponseEntity.ok(libroActualizado);
+    } else {
+        return ResponseEntity.notFound().build();
     }
+}
+
+    }
+    
+        
+    
     
 
 
