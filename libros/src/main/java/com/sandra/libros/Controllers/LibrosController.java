29a5fc44b31daf2e4a72f9962cd98993f299ca3c
Sandra631 <sandra.yolotzin.reyes.garcia.com>
2025-05-18 17:49:10 -0600

    Act7-Creando metodo DELETE en el controlador

--- libros/src/main/java/com/sandra/libros/Controllers/LibrosController.java
+++ libros/src/main/java/com/sandra/libros/Controllers/LibrosController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import java.util.Optional;
 
@@ -37,6 +38,7 @@ public class LibrosController {
 public LibrosModel insertarLibros(@RequestBody LibrosModel libro) {
     return librosRepository.save(libro);
 }
+
 //Metodo para editar un libro a la base de datos
     @PutMapping("/editar-libro/{id}")
 public ResponseEntity<LibrosModel> editarLibro(@PathVariable Long id, @RequestBody LibrosModel libroActualizado) {
@@ -55,6 +57,12 @@ public ResponseEntity<LibrosModel> editarLibro(@PathVariable Long id, @RequestBo
     } else {
         return ResponseEntity.notFound().build();
     }
+}
+
+ // Método para eliminar un libro 
+@DeleteMapping("/eliminar-libro/{id}")
+public void eliminarLibro(@PathVariable Long id){
+    librosRepository.deleteById(id);
 }
 
     }
