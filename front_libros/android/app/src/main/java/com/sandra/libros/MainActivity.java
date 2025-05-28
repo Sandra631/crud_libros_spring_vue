94a9aa2cfa0d90f5230ff7d8a05f009685ce2b7a
Sandra631 <sandra.yolotzin.reyes.garcia.com>
2025-05-28 01:10:01 -0600

    cambios

---
+++ front_libros/android/app/src/main/java/com/sandra/libros/MainActivity.java
@@ -0,0 +1,5 @@
+package com.sandra.libros;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ front_libros/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.sandra.libros',
+  appName: 'GestionLibros',
+  webDir: 'dist'
+};
+
+export default config;
